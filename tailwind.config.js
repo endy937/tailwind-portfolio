2b7c883d25fba62fb0ad3cb5a6131db1524f70ec
Endy Muhamad <endymuhamad93@gmail.com>
2025-08-29 16:38:45 +0700

    header and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        // dark: "#0f172a",
+        dark: "#18191a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
